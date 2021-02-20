5a3b46af881f85d881fa5eccc7747d35a7889154
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-02-20 23:16:06 +0100

    Add feature first draft

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
+    [SerializeField] private Player player;
+    [SerializeField] private float followThreshold = 0.5f;
+    [SerializeField] private float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float playerXPositionViewPort = Camera.main.WorldToViewportPoint(player.transform.position).x;
+        if (playerXPositionViewPort >= followThreshold)
+        {
+            Vector3 goalPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+            Camera.main.transform.position = Vector3.SmoothDamp(transform.position, goalPosition, ref velocity, smoothTime);
+        }
     }
 }
