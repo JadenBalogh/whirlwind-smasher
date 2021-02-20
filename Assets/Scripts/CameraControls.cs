9ae57a022622f3be180cf538d43778f9326dd6a8
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-02-21 00:43:14 +0100

    Add back wall

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraControls : MonoBehaviour
 {
     private Player player;
+    GameObject wall;
     [SerializeField] private float followThreshold = 0.5f;
     [SerializeField] private float smoothTime = 0.3f;
     private Vector3 velocity = Vector3.zero;
@@ -12,6 +13,7 @@ public class CameraControls : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        wall = GameObject.FindGameObjectWithTag("Wall");
     }
 
     void Update()
@@ -19,8 +21,14 @@ public class CameraControls : MonoBehaviour
         float playerXPositionViewPort = Camera.main.WorldToViewportPoint(player.transform.position).x;
         if (playerXPositionViewPort >= followThreshold)
         {
-            Vector3 goalPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
-            transform.position = Vector3.SmoothDamp(transform.position, goalPosition, ref velocity, smoothTime);
+            // Move camera forward to follow the player
+            Vector3 cameraTargetPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, cameraTargetPosition, ref velocity, smoothTime);
+
+            // Move the wall forward 
+            // TODO: set the below numbers to be dynamically-obtained values (or have constants set more formally)
+            Vector3 wallTargetPosition = new Vector3(transform.position.x - 8.4f, 4, 10);
+            wall.transform.position = wallTargetPosition;
         }
     }
 }
