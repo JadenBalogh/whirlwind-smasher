ea2b590a7b0d447e52748ba656e6852c682e01b9
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-02-21 16:58:49 +0100

    Add spawn probability

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,6 +7,7 @@ public class Spawner : MonoBehaviour
     [SerializeField] MonoBehaviour[] prefabs;
     [SerializeField] private float minDist;
     [SerializeField] private float maxDist;
+    [SerializeField] private float probability;
     private float nextX;
     private ArrayList spawnables = new ArrayList();
 
@@ -22,8 +23,12 @@ public class Spawner : MonoBehaviour
             // Update nextX
             nextX = Camera.main.ViewportToWorldPoint(Vector2.right).x + Random.Range(minDist, maxDist);
 
-            // Pick a random prefab to spawn
-            spawnables.Add(Instantiate(prefabs[0], new Vector2(nextX, -3.425f), Quaternion.identity));
+            if (Random.Range(0f, 1f) <= probability)
+            {
+                // Pick a random prefab to spawn
+                int index = Random.Range(0, prefabs.Length);
+                spawnables.Add(Instantiate(prefabs[index], new Vector2(nextX, -3.425f), Quaternion.identity));
+            }
         }
     }
 }
