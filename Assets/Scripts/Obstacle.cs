d7c2751cf5ca2f6d9c2bbe973086bb0494e2ea14
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-02-21 17:30:04 +0100

    Add off screen destruction

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameObject.AddComponent(typeof(DestroyOnLeaveScreen));
     }
 }
