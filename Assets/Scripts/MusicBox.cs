bd7ed662fdfa3e9f5653c0f4c2582f84daace9f3
Jaden Balogh <jadenbalogh@gmail.com>
2021-02-21 11:13:30 -0800

    Add button sounds

--- Assets/Scripts/MusicBox.cs
+++ Assets/Scripts/MusicBox.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class MusicBox : MonoBehaviour
 {
+    private static MusicBox instance;
+
     void Awake()
     {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
         DontDestroyOnLoad(gameObject);
     }
 }
